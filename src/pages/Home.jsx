91597aa9090e90138b5ec88525c115c80b42f441
seongjun1208 <skwid17@naver.com>
2024-11-13 13:20:31 +0900

    1113update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,27 +15,39 @@ const GlassBoard = styled.div`
   position:absolute;
   top:22vh;
   right:24vw;
-  width:460px;
-  height:350px;
+  width:24vw;
+  height:32vh;
   border-radius:25px;
   background: linear-gradient(135deg,rgba( 255, 255, 255, 0.3 ),rgba(0, 0, 0, 0.1) ) ;
   box-shadow: 0 6px 15px 0 rgba( 131, 174, 255, 0.2 );
   backdrop-filter: blur( 4px );
   -webkit-backdrop-filter: blur( 4px );
   border: 1px solid rgba( 255, 255, 255, 0.1 );
+  display:flex;
+  justify-content: center;
+  align-items: center;
 `
 const GlassBoardLeft = styled(GlassBoard)`
-  width:314px;
-  height:190px;
-  top:26vh;
+  width:16vw;
+  height:17.5vh;
+  top:25.5vh;
   right:0vw;
-  left:24vw;
+  left:16vw;
   border-radius:25px;
   background: linear-gradient(135deg,rgba( 255, 255, 255, 0.3 ),rgba(0, 0, 0, 0.1) ) ;
   box-shadow: 0 6px 15px 0 rgba( 131, 174, 255, 0.2 );
   backdrop-filter: blur( 4px );
   -webkit-backdrop-filter: blur( 4px );
   border: 1px solid rgba( 255, 255, 255, 0.1 );
+
+`
+
+const TextContainer = styled.div`
+  display:flex;
+  flex-direction:column;
+`
+const Name = styled.h2`
+
 `
 
 const Home = () => {
@@ -43,7 +55,11 @@ const Home = () => {
     <Container>
       <HomeBackground />
       <GlassBoardLeft />
-      <GlassBoard>BOX</GlassBoard>
+      <GlassBoard>
+        <div>
+          <h2>JI SEONG JUN</h2>
+        </div>
+      </GlassBoard>
     </Container>
   )
 }
